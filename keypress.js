72702165b89454583a07d713dc0235548378f267
Slovak_Cat <Slovak_Cat@boun.cr>
2020-04-17 23:37:51 +0200

    📦 NEW: all Ios icons added (short of)

--- keypress.js
+++ keypress.js
@@ -2,10 +2,10 @@ var altKey = isKeyPressed(event)
 function isKeyPressed(event) {
     if (event.altKey) {
       alert ("say waaaaaaaat! and press ok");
-      location.href='https://forms.gle/gVu5wWhwAq3SQcRG8';
+      location.href='https://slovak-cat.github.io/iicon/c.html';
       console.log('Starting EasterEgg');;
     } else {
-      console.log("🐈! try it with alt key and click");
+      console.log("🐈! try it with alt key");
     }
   }
 // meow
