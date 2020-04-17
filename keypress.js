c57dd4c8ee0c2754b5898fa06536ddaae9b11cb1
Slovak_Cat <Slovak_Cat@boun.cr>
2020-04-17 11:42:07 +0200

    📦 NEW: Key Shortcuts

---
+++ keypress.js
@@ -0,0 +1,11 @@
+var altKey = isKeyPressed(event)
+function isKeyPressed(event) {
+    if (event.altKey) {
+      alert ("say waaaaaaaat! and press ok");
+      location.href='https://forms.gle/gVu5wWhwAq3SQcRG8';
+      console.log('Starting EasterEgg');;
+    } else {
+      console.log("🐈! try it with alt key and click");
+    }
+  }
+// meow
